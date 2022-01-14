4879ebade3c5be884e6a866b5b3b807e5dc6369b
braneeth <43222404+braneeth13@users.noreply.github.com>
2022-01-14 11:00:54 -0500

    Create complexNumber.java

---
+++ complexNumber.java
@@ -0,0 +1,27 @@
+public class complexNumber implements number {
+
+	@Override
+	public number add(number num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public number subtract(number num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public number multiply(number num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public number divide(number num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
